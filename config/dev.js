630670b669751e864698fab113bc7e261e70ac1b
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-11-10 16:04:40 +0530

    new

---
+++ config/dev.js
@@ -0,0 +1,24 @@
+module.exports = {
+  NODE_ENV: 'development',
+  NEW_PORT: 5000,
+  MONGO_URI:
+    // 'mongodb+srv://manipal:manipal123@muj-convocation-2021.cctdf.mongodb.net/muj-convocation-2021?retryWrites=true&w=majority',
+    // 'mongodb+srv://manipal:ankit1690@manipal-convocation.inwzf.mongodb.net/muj-convocation?retryWrites=true&w=majority',
+    // 'mongodb+srv://manipal:manipal123@manipal-convocation-202.yjwqzma.mongodb.net/Manipal-Convocation-2022?retryWrites=true&w=majority',
+    // 'mongodb+srv://mujconvocation2k:OO1CIrYDD5UomJpy@cluster0.xhmftui.mongodb.net/?retryWrites=true&w=majority',
+        // 'mongodb+srv://muj-convo:muj123@muj-convocation-2023.y6reuaf.mongodb.net/?retryWrites=true&w=majority',
+        'mongodb+srv://muj-convo:mujConvo@muj-convocation-2023.y6reuaf.mongodb.net/?retryWrites=true&w=majority',
+  JWT_SECRET: 'qewretrytfgbui', 
+  JWT_COOKIE_EXPIRE: 30,
+  NODE_ENV: 'production',
+  SMTP_HOST: 'smtp.gmail.com',
+  SMTP_PORT: '587',
+  SMTP_EMAIL: 'manipalconvocation@gmail.com',
+  FROM_EMAIL: 'no-reply@mujconvocation.in',
+  FROM_NAME: 'MUJ Convocation',
+  SMTP_PASSWORD: 'wjrjwsupdwtgqilv',
+  PAYUMONEY_MERCHENT_KEY: 'kqfd6O',
+  PAYUMONEY_SALT_VALUE: 'n0Qpybgx',
+  SENDGRID_API_KEY:
+    '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', // SendGrid Password: mujmujmujmujmujmuj@2022
+};
